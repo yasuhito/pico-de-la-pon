57e3b492205d6d8ede07ebc0b58fb24136137aef
Yasuhito Takamiya <yasuhito@gmail.com>
2022-12-28 15:32:18 +0900

    カーソルを表示

--- src/main_main.lua
+++ src/main_main.lua
@@ -1,18 +1,24 @@
 ---@diagnostic disable: lowercase-global
 
 local stack_class = require("stack")
+local player_cursor_class = require("player_cursor")
+
 local stack
+local player_cursor
 
 function _init()
   stack = stack_class()
   stack:put_random_blocks()
+  player_cursor = player_cursor_class()
 end
 
 function _update60()
+  player_cursor:update()
 end
 
 function _draw()
   cls()
 
   stack:draw()
+  player_cursor:draw(3, 3)
 end
